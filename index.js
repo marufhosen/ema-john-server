90d307ca743aad90eee39c05b2ae7d0ecf712a7e
Maruf Islam <maruf15-1591@diu.edu.bd>
2021-08-28 18:09:46 +0600

    work with ema-john server

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lvals.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const productsCollection = client.db("emaJohnStore").collection("products");
+  const orderCollection = client.db("emaJohnStore").collection("orders");
+
+  app.post("/addProduct", (req, res) => {
+    const products = req.body;
+    productsCollection.insertOne(products).then((res) => {
+      res.send(res.insertedCount);
+    });
+  });
+  app.get("/products", (req, res) => {
+    productsCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.get("/product/:key", (req, res) => {
+    productsCollection
+      .find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+  app.post("/productsByKeys", (req, res) => {
+    const productKey = req.body;
+    productsCollection
+      .find({ key: { $in: productKey } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    orderCollection.insertOne(order).then((res) => {
+      res.send(res.insertedCount > 0);
+    });
+  });
+});
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.listen(5000);
